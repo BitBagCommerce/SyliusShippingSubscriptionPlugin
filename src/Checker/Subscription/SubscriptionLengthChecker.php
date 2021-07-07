a77287d6c92316274dbe5621fc9c739d75c86759
pszost <piotr.szost99@gmail.com>
2021-07-07 14:25:12 +0200

    Update README & Fix length checker

--- src/Checker/Subscription/SubscriptionLengthChecker.php
+++ src/Checker/Subscription/SubscriptionLengthChecker.php
@@ -21,8 +21,8 @@ final class SubscriptionLengthChecker implements SubscriptionLengthCheckerInterf
 
     public function checkSubscriptionLength(OrderInterface $order): int
     {
-        return 0;
-        //$units = $this->orderItemUnitRepository->findUnitsWithProductShippingSubscription($order);
+
+        $units = $this->orderItemUnitRepository->findUnitsWithProductShippingSubscription($order);
 
         if (count($units) === 0) {
             return 0;
