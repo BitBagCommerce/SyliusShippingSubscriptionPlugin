048f39d78774879610bcce8e8283d1928b1b3383
Piotr Szostakiewicz <63583880+pszost@users.noreply.github.com>
2021-07-12 16:10:17 +0200

    Fix PHPstan (#5)
    * Fix PHPstan
    
    * Update readme

--- tests/Application/src/Entity/Customer/Customer.php
+++ tests/Application/src/Entity/Customer/Customer.php
@@ -4,18 +4,20 @@ declare(strict_types=1);
 
 namespace Tests\BitBag\SyliusShippingSubscriptionPlugin\Entity\Customer;
 
-use BitBag\SyliusShippingSubscriptionPlugin\Entity\ShippingSubscription;
+use BitBag\SyliusShippingSubscriptionPlugin\Entity\ShippingSubscriptionInterface;
 use BitBag\SyliusShippingSubscriptionPlugin\Entity\SubscriptionAwareInterface;
 use Doctrine\Common\Collections\Collection;
 use Sylius\Component\Core\Model\Customer as BaseCustomer;
 
 class Customer extends BaseCustomer implements SubscriptionAwareInterface
 {
-    /** @var Collection|ShippingSubscription[] */
+    /** @var Collection<int, ShippingSubscriptionInterface>|null */
     protected $shippingSubscriptions;
 
+    /** @return Collection<int, ShippingSubscriptionInterface>|null */
     public function getSubscriptions(): ?Collection
     {
         return $this->shippingSubscriptions;
     }
 }
+
