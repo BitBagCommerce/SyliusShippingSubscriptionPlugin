20b69cc87829c7c75a68d13d41e21d7ffdf7cd91
Gabriela Lubkowska <dsw45017@student.dsw.edu.pl>
2023-03-23 15:45:07 +0100

    Adding Sylius 1.12 support

--- tests/Application/config/sylius/1.11/bundles.php
+++ tests/Application/config/sylius/1.11/bundles.php
@@ -2,6 +2,8 @@
 
 return [
     BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     SyliusLabs\Polyfill\Symfony\Security\Bundle\SyliusLabsPolyfillSymfonySecurityBundle::class => ['all' => true],
     Sylius\Calendar\SyliusCalendarBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
